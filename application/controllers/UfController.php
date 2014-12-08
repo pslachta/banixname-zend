d9ab5616e4c1ddc52c7470acb20f0e06489d914f
Petr Slachta <petr.slachta@lundegaard.eu>
2014-12-08 16:51:54 -0500

    added uf controller

---
+++ application/controllers/UfController.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of AboutUsController
+ *
+ * @author banix
+ */
+class UfController extends Zend_Controller_Action {
+	
+	public function init() {
+		/* Initialize action controller here */
+	}
+
+	public function indexAction() {
+		echo "Ultra Fun";
+	}
+}
