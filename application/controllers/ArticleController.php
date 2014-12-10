97b7103248a7f7b4103e6ec509d6c8d352ae5761
Petr Slachta <petr.slachta@lundegaard.eu>
2014-12-09 21:57:39 -0500

    maintenance articles components

--- application/controllers/ArticleController.php
+++ application/controllers/ArticleController.php
@@ -13,51 +13,64 @@
  */
 class ArticleController extends Zend_Controller_Action {
 
+//	var $article = [
+//		'id' => 0,
+//		'author' => '-b-n-x-',
+//		'email' => 'banix@centrum.cz',
+//		'body' => 'Testovaci text',
+//		'name' => 'Pokusny clanek',
+//		'category' => 'testovaci',
+//		'directory' => 'ropice',
+//	];
+//	var $artImages = [
+//		['id' => 1,'img' => 'dawnw.jpg','thumb' => 'dawnic.jpg','title' => 'Slunce nad mořem','desc'=>'V tuto chvíli jsme si připadali jako nějakém souostroví'],
+//		['id' => 1,'img' => 'dawn2w.jpg','thumb' => 'dawn2ic.jpg','title' => 'Východ','desc'=>'Všude dokola jenom moře a semtam nějaký ostrůvek.'],
+//		['id' => 1,'img' => 'morew.jpg','thumb' => 'moreic.jpg','title' => 'Mlhopad','desc'=>'Dokonce v Tyrském údolí bylo tenkrát moře výše než v Řeckém.'],
+//		['id' => 1,'img' => 'woodw.jpg','thumb' => 'woodic.jpg','title' => 'Temný les','desc'=>'Takto vypadá zatopený les asi 8 m pod hladinou.'],
+//	];
+
 	public function init() {
 		/* Initialize action controller here */
 	}
 
 	public function indexAction() {
-		echo "Article - index()";
+//		echo "Article - index()";
 	}
 
-	public function viewAction($id = 21) {
-		echo "Article - view($id)";
-		echo "<tr><td align=\"center\"><h3>V?chod slunce</h3></td></tr><tr><td align=\"left\">Koncem zimy 00/01 jsme si za?li s k?mo?em na vrchol Ropice. R?no n?s p?ekvapilo hodn? dobr?mi ml?n?mi ?kazy. K?mo? to sv?m nikonem vyfotil a te? se na to m??ete taky kouknout. </td></tr><tr><td align=\"center\"><a href=\"javascript:closeWindow();openWindow1('/picone.php?id=144&dire=ropice',416,324)\"><img src=\"/clanky/ropice/thumb/dawnic.jpg\" border=\"0\" alt=\"Slunce nad mo?em\"></a> <a href=\"javascript:closeWindow();openWindow1('/picone.php?id=145&dire=ropice',416,328)\"><img src=\"/clanky/ropice/thumb/dawn2ic.jpg\" border=\"0\" alt=\"V?chod\"></a> <a href=\"javascript:closeWindow();openWindow1('/picone.php?id=146&dire=ropice',466,362)\"><img src=\"/clanky/ropice/thumb/moreic.jpg\" border=\"0\" alt=\"Mlhopad\"></a> <a href=\"javascript:closeWindow();openWindow1('/picone.php?id=147&dire=ropice',375,610)\"><img src=\"/clanky/ropice/thumb/woodic.jpg\" border=\"0\" alt=\"Temn? les\"></a> </td></tr><tr><td align=\"right\"><a href=\"mailto:banix@centrum.cz\">-b-n-x-</a></td></tr>";
+	public function viewAction() {
+		$this->articles = new Application_Model_DbTable_Articles();
+		$this->authors = new Application_Model_DbTable_Authors();
+		$this->images = new Application_Model_DbTable_Images();
+		$id = $this->getRequest()->getParam('id');
+		$this->view->article = $this->getArticle($id);
+		$this->view->articleImages = $this->getArticleImages($id);
 	}
 
-	/*
-	 * Old solution
-	 * 
-	  if(!$id) $id=$_GET['id'];
-	  $conn=mysql_connect($dbhost,$dbuser,$dbpass);
-	  mysql_select_db($dbname);
-	  $sqls="select nazev,txt,autor.autor,autor.email,dire,kat from clanky left join autor on clanky.autor=autor.uid where clanky.id=$id";
-	  $result=mysql_query($sqls,$conn);
-	  $pole=mysql_fetch_row($result);
-	  $nazev=$pole[0];
-	  $txt=$pole[1];
-	  $autor=$pole[2];
-	  $email=$pole[3];
-	  $dire=$pole[4];
-	  $kategory=$pole[5];
+	private function getArticle($id) {
+		$clanek = $this->articles->getArticle($id);
+		$article['name'] = $clanek['nazev'];
+		$article['body'] = $clanek['txt'];
+		$article['category'] = $clanek['kat'];
+		$article['directory'] = $clanek['dire'];
+		$article['id'] = $clanek['id'];
+		$author = $this->authors->getAuthor($clanek['autor']);
+		$article['author'] = $author['autor'];
+		$article['email'] = $author['email'];
+		return $article;
+	}
 
-	  echo "<tr><td align='center'><h3>$nazev</h3></td></tr>";
-	  echo "<tr><td align='left'>$txt</td></tr>";
-	  $sqls="select id,img,thumb,popisek from obr where clanid=$id";
-	  $result=mysql_query($sqls,$conn);
-	  echo "<tr><td align='center'>";
-	  while($pole=mysql_fetch_row($result))
-	  {
-	  $id=$pole[0];
-	  $imgs="$HOME/clanky/".$dire."/".$pole[1];
-	  $thumb="$HOME/clanky/".$dire."/thumb/".$pole[2];
-	  $popisek=$pole[3];
-	  echo imgwin("$HOME/picone.php?id=$id&dire=$dire",$imgs,$thumb,$popisek)." ";
-	  }
-	  mysql_close($conn);
-	  echo "</td></tr>
-	  <tr><td align='right'><a href='mailto:$email'>$autor</a></td></tr>";
-	  ?>
-	 */
+	private function getArticleImages($id) {
+		$obrazky = $this->images->fetchAll("clanid = $id");
+		$articleImages = [];
+		foreach ($obrazky as $obrazek) {
+			$image['id'] = $obrazek->id;
+			$image['image'] = $obrazek->img;
+			$image['thumb'] = $obrazek->thumb;
+			$image['title'] = $obrazek->popisek;
+			$image['description'] = $obrazek->txt;
+			$articleImages[] = $image;
+		}
+		return $articleImages;
+	}
+
 }
